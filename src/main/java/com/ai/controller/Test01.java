ad61d99c494105c997c04d6be2b3fdbbc6acb001
LNB <13837576717@163.com>
2021-10-20 10:56:59 +0800

    test

---
+++ src/main/java/com/ai/controller/Test01.java
@@ -0,0 +1,14 @@
+package com.ai.controller;
+
+import ch.qos.logback.core.pattern.FormatInfo;
+import org.springframework.boot.context.properties.source.IterableConfigurationPropertySource;
+
+import java.sql.SQLOutput;
+
+public class Test01 {
+    public void test01(){
+        System.out.println("test");
+    }
+
+
+}
---
+++ src/main/java/com/ai/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.ai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class TestController {
+    @RequestMapping("/world")
+    public String say(){
+        return "test";
+    }
+}
